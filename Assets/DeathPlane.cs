48437aa5ad9b61452e0845bed947b1c0801ca47d
DeanSmith2999 <101359558+DeanSmith2999@users.noreply.github.com>
2022-03-15 14:22:33 -0400

    Actually added camera movement and respawn scripts

---
+++ Assets/DeathPlane.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnpoint;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        player.transform.position = respawnpoint.transform.position;
+    }
+
+
+}
